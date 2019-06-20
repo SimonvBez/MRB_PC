53de81dabf2feabe6c8029222dd38c48f568e6da
SimonvBez <31727669+SimonvBez@users.noreply.github.com>
2019-06-21 01:50:11 +0200

    Now keeps track of the current servo values, and only message the Arduino when they change

--- MRB_Serial.py
+++ MRB_Serial.py
@@ -3,12 +3,13 @@ import serial.tools.list_ports
 import time
 
 
-class MRB_Controller:
+class MRB_Serial:
     def __init__(self):
         self.ser = serial.Serial()
         self.ser.baudrate = 2400
         # self.ser.setDTR(0)
         self.wait_for_connection()
+        self.servo_times = [None] * 16
 
     def __del__(self):
         self.close()
@@ -39,15 +40,21 @@ class MRB_Controller:
         self.ser.write(b'Cmd' + cmd_bytes)
 
     def reset_servos(self):
-        self.send_serial_command(b'R')
+        if self.servo_times != [None] * 16:
+            self.send_serial_command(b'R')
+            self.servo_times = [None] * 16
 
     def set_servo(self, servo_num, end_time):
         if 0 <= end_time <= 4095 and 0 <= servo_num <= 15:
-            self.send_serial_command(b'S' + bytes([servo_num]) + end_time.to_bytes(2, byteorder='big'))
+            if end_time != self.servo_times[servo_num]:
+                self.send_serial_command(b'S' + bytes([servo_num]) + end_time.to_bytes(2, byteorder='big'))
+                self.servo_times[servo_num] = end_time
 
     def set_servos(self, end_times):
         if max(end_times) <= 4095 and min(end_times) >= 0 and len(end_times) == 3:
-            self.send_serial_command(b'A' + self.int_list2bytes(end_times))
+            if end_times != self.servo_times:
+                self.send_serial_command(b'A' + self.int_list2bytes(end_times))
+                self.servo_times = end_times
 
     def set_servo_percentage(self, servo_num, percentage):
         self.set_servo(servo_num, 550 - int(percentage * 1.5))
