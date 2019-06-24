798df8020f8de8e7361669d9a09d92d573f3eb92
SimonvBez <31727669+SimonvBez@users.noreply.github.com>
2019-06-24 13:04:23 +0200

    Implemented servo position and ball destination selection

--- MRB_Controller.py
+++ MRB_Controller.py
@@ -19,6 +19,10 @@ class MRB_Controller():
         self.video_cap = None
         self.gui = None
         self.servo_offsets = [0] * 3
+        self.servo_positions = [(None, None)] * 3
+        self.servo_calibration_counter = 0
+        self.ball_destination = (None, None)
+        self.ball_position = (None, None)
         self.arduino = None
 
     def set_gui(self, gui):
@@ -34,9 +38,14 @@ class MRB_Controller():
             self.lower_orange = (hsv_pixel - np.array([4, 100, 30])).clip(0, 255)
             self.upper_orange = (hsv_pixel + np.array([4, 255, 255])).clip(0, 255)
         elif select_mode == MRB_Constants.SELECT_SERVO_POS:
-            print("Servo cal")
+            if not self.servo_calibration_counter:
+                self.servo_positions = [(None, None)] * 3
+            self.servo_positions[self.servo_calibration_counter] = (event.x, event.y)
+            self.servo_calibration_counter += 1
+            if self.servo_calibration_counter == 3:
+                self.servo_calibration_counter = 0
         elif select_mode == MRB_Constants.SELECT_BALL_DEST:
-            print("Ball dest")
+            self.ball_destination = (event.x, event.y)
 
     def cam_thread(self):
         self.video_cap = cv2.VideoCapture(0)
@@ -54,17 +63,33 @@ class MRB_Controller():
                 contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                 contours = imutils.grab_contours(contours)
 
+                # Find and draw ball position
                 if len(contours) > 0:
                     c = max(contours, key=cv2.contourArea)
                     ((x, y), radius) = cv2.minEnclosingCircle(c)
                     M = cv2.moments(c)
                     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
                     if radius > 10:
+                        self.ball_position = center
                         cv2.circle(bgr, (int(x), int(y)), int(radius), (0, 255, 255), 2)
                         cv2.circle(bgr, center, 5, (0, 0, 255), -1)
 
+                # Draw servo positions
+                for i in range(len(self.servo_positions)):
+                    if self.servo_positions[i][0] is not None:
+                        text_position = list(self.servo_positions[i])
+                        text_position[0] -= 10
+                        text_position[1] += 10
+                        text_position = tuple(text_position)
+                        cv2.putText(bgr, str(i + 1), text_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+                # Draw ball destination
+                if self.ball_destination[0] is not None:
+                    cv2.circle(bgr, self.ball_destination, 10, (60, 255, 0), 2)
+
                 mask = cv2.resize(mask, None, fx=0.5, fy=0.5)
 
+                # Put it all in the GUI
                 rgb_photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)))
                 mask_photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(mask))
                 self.gui.get_top().Webcam.create_image(0, 0, image=rgb_photo, anchor='nw')
@@ -92,10 +117,10 @@ class MRB_Controller():
                     time.sleep(0.1)
                 elif state == MRB_Constants.STATE_CALIBRATION:
                     self.set_servo_percentages_with_offset([0, 0, 0])
-                    time.sleep(0.1)
+                    time.sleep(0.05)
                 elif state == MRB_Constants.STATE_IDLE:
                     self.arduino.reset_servos()
-                    time.sleep(0.1)
+                    time.sleep(0.2)
 
             except (serial.serialutil.SerialException, serial.serialutil.SerialTimeoutException):
                 self.gui.set_connected(False)
